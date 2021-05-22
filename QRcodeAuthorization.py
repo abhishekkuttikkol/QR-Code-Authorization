2cebd2187f93c2a1cd1a53b372c3af303aed2cd4
abhishekkuttikkol <75170385+abhishekkuttikkol@users.noreply.github.com>
2021-05-22 15:37:01 +0530

    Add files via upload

---
+++ QRcodeAuthorization.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+
+# img = cv2.imread('qrcode.jpg')
+cap = cv2.VideoCapture(0)
+# cap.open('http://192.168.1.203:8080/video')
+
+with open('myDataFile.txt') as f:
+    myDataList = f.read().splitlines()
+
+print(myDataList)
+
+while cap.isOpened():
+    success, img = cap.read()
+
+    for barcode in decode(img):
+        print(barcode.data)
+        myData = barcode.data.decode('utf')
+        print(myData)
+
+        if myData in myDataList:
+            myOutput = 'Authorized'
+            outputColor = (0, 255, 0)
+        else:
+            myOutput = 'Unauthorized'
+            outputColor = (0, 0, 255)
+
+        points = np.array([barcode.polygon], np.int32)
+        points = points.reshape((-1, 1, 2))
+        cv2.polylines(img, [points], True, outputColor, 5)
+        points2 = barcode.rect
+        cv2.putText(img, myOutput, (points2[0], points2[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, outputColor,3)
+
+    cv2.imshow('scanner',img)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+cap.release()
